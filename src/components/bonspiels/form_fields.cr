70106ca3662753c6418dda8be5c466194c592a21
Alex Piechowski <alex@piechowski.io>
2022-01-22 08:30:03 -0600

    Minimum sheets 1

--- src/components/bonspiels/form_fields.cr
+++ src/components/bonspiels/form_fields.cr
@@ -2,10 +2,8 @@ class Bonspiels::FormFields < BaseComponent
   needs operation : SaveBonspiel
 
   def render
-    mount Shared::Field, operation.name, &.text_input(autofocus: "true")
-
     mount Shared::Field, operation.start_at, &.date_input
     mount Shared::Field, operation.end_at, &.date_input
-    mount Shared::Field, operation.sheets, &.number_input(min: 0)
+    mount Shared::Field, operation.sheets, &.number_input(min: 1)
   end
 end
