efa2a7db5fcec2b731d43881b4a8c8ca75df93b1
Alex Piechowski <alex@piechowski.io>
2022-01-22 10:03:03 -0600

    Add todo for timing

--- src/models/bonspiel.cr
+++ src/models/bonspiel.cr
@@ -2,7 +2,7 @@ class Bonspiel < BaseModel
   table do
     column name : String
     column start_at : Time # TODO: Start at must come before end_at
-    column end_at : Time
+    column end_at : Time   # TODO: automatically make it 23:59:59:59:59
     column sheets : Int16
     belongs_to owner : User
     has_many draws : Draw
