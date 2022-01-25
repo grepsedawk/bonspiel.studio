ea0861378b6ddc87b365e7712e5717cbcf941978
Alex Piechowski <alex@piechowski.io>
2022-01-24 22:26:03 -0600

    Only send what is needed over the wire, not the whole template

--- src/pages/games/update_page.cr
+++ src/pages/games/update_page.cr
@@ -1,9 +1,10 @@
-class Games::UpdatePage < MainLayout
+class Games::UpdatePage
+  include Lucky::HTMLPage
   needs game : Game
   needs operation : SaveGame
   quick_def page_title, "Game with id: #{game.id}"
 
-  def content
+  def render
     mount Games::EditCard, operation: operation, game: game
   end
 end
