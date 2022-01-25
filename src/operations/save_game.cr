1a2d0fad48febfc1e8f6944b6ec13731f27c335e
Alex Piechowski <alex@piechowski.io>
2022-01-24 21:04:56 -0600

    Formatting

--- src/operations/save_game.cr
+++ src/operations/save_game.cr
@@ -2,6 +2,12 @@ class SaveGame < Game::SaveOperation
   # To save user provided params to the database, you must permit them
   # https://luckyframework.org/guides/database/saving-records#perma-permitting-columns
   #
-  permit_columns team_a_score, team_b_score, team_a_name, team_b_name, team_a_club_name, team_b_club_name, current_end
+  permit_columns team_a_score,
+    team_b_score,
+    team_a_name,
+    team_b_name,
+    team_a_club_name,
+    team_b_club_name,
+    current_end
   upsert_unique_on :sheet, :draw_id
 end
