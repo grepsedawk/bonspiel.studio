220f375d073bed0d872578de50d3dafcdc7c3f8b
Alex Piechowski <alex@piechowski.io>
2022-01-24 21:25:25 -0600

    Add mockup for hammer and final

--- src/components/games/edit_card.cr
+++ src/components/games/edit_card.cr
@@ -7,23 +7,32 @@ class Games::EditCard < BaseComponent
       div class: "card card-bordered mt-6" do
         div class: "card-body" do
           h2 "Sheet #{game.sheet}", class: "card-title"
-          div class: "stats" do
+          div class: "stats grid-flow-col" do
             div class: "stat place-items-center place-content-center" do
-              mount Games::AutoSavingInput, operation.team_a_name, game: game
+              mount Games::AutoSavingInput, operation.team_a_name, game: game, append_class: "stat-title"
               mount Games::IncrementingStatValue, operation.team_a_score, game: game
               mount Games::AutoSavingInput, operation.team_a_club_name, game: game, append_class: "stat-desc"
+              div class: "stat-actions" do
+                span "Hammer", class: "btn btn-ascent"
+              end
             end
             div class: "stat place-items-center place-content-center" do
               div "End", class: "stat-title"
               mount Games::IncrementingStatValue, operation.current_end, game: game
               div class: "stat-desc" do
-                text "Game started at: 20:20:20"
+                text "Good Curling!"
+              end
+              div class: "stat-actions" do
+                span "Final", class: "btn btn-secondary"
               end
             end
             div class: "stat place-items-center place-content-center" do
-              mount Games::AutoSavingInput, operation.team_b_name, game: game
+              mount Games::AutoSavingInput, operation.team_b_name, game: game, append_class: "stat-title"
               mount Games::IncrementingStatValue, operation.team_b_score, game: game
               mount Games::AutoSavingInput, operation.team_b_club_name, game: game, append_class: "stat-desc"
+              div class: "stat-actions" do
+                span "Hammer", class: "btn btn-ascent"
+              end
             end
           end
         end
