06a326487493e02ec97cab61cf598728318cc0b6
Alex Piechowski <alex@piechowski.io>
2022-01-27 14:35:57 -0600

    Make scoreboard more managable

--- src/pages/bonspiels/scoreboard/index_page.cr
+++ src/pages/bonspiels/scoreboard/index_page.cr
@@ -4,22 +4,13 @@ class Bonspiels::Scoreboard::IndexPage < ScoreboardLayout
 
   def content
     h2 "Sheet #{game.sheet}"
-    div class: "stats" do
-      div class: "stat place-items-center place-content-center" do
-        div game.team_a_name.to_s, class: "stat-title"
-        div game.team_a_score.to_s, class: "stat-value"
-        div game.team_a_club_name.to_s, class: "stat-desc"
-      end
-      div class: "stat place-items-center place-content-center" do
-        div "End", class: "stat-title"
-        div game.current_end.to_s, class: "stat-value"
-        div "Good Curling!", class: "stat-desc"
-      end
-      div class: "stat place-items-center place-content-center" do
-        div game.team_b_name.to_s, class: "stat-title"
-        div game.team_b_score.to_s, class: "stat-value"
-        div game.team_b_club_name.to_s, class: "stat-desc"
+    div class: "grid grid-cols-2 max-w-xs" do
+        div game.team_a_name.to_s
+        div game.team_a_score.to_s
+        div game.team_b_name.to_s
+        div game.team_b_score.to_s
+        div "End"
+        div game.current_end.to_s
       end
     end
-  end
 end
