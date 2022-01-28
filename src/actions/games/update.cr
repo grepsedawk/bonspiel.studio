44a0877d3121f1e2b325999031bd4c841297e626
Alex Piechowski <alex@piechowski.io>
2022-01-28 03:02:12 -0600

    Simplify game update
    Presenting now happens elsewhere

--- src/actions/games/update.cr
+++ src/actions/games/update.cr
@@ -3,19 +3,7 @@ class Games::Update < BrowserAction
     game = GameQuery.find(game_id)
 
     SaveGame.update(game, params) do |operation, updated_game|
-      if top?
-        redirect Draws::Show.with(game.draw_id)
-      else
-        html UpdatePage, operation: operation, game: updated_game
-      end
+      html UpdatePage, operation: operation, game: updated_game
     end
-  end
-
-  private def top?
-    params.to_h["game"]["presenting"]
-
-    true
-  rescue KeyError
-    false
   end
 end
