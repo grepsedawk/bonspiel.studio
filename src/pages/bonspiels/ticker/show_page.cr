c35212db923ab0e095e554bbfee3e8c48788e7e7
Alex Piechowski <alex@piechowski.io>
2022-01-30 15:31:34 -0600

    Shrink scoreboard height, but leave font the same size

--- src/pages/bonspiels/ticker/show_page.cr
+++ src/pages/bonspiels/ticker/show_page.cr
@@ -1,17 +1,17 @@
 class Bonspiels::Ticker::ShowPage < Bonspiels::Scoreboard::IndexPage
   def content
     tag "turbo-frame", id: "game-scoreboard-#{game.id}" do
-        div class: "flex text-center gap-2 animate-slide-in" do
+        div class: "scoreboard flex text-center gap-2 animate-slide-in" do
         div class: "btn-group flex-1 flex" do
           hammer_button if game.team_a_hammer?
-          div game.team_a_name.to_s, class: "flex-1 btn md:btn-lg lg:btn-xl"
-          div game.team_a_score.to_s, class: "btn md:btn-lg lg:btn-xl"
+          div game.team_a_name.to_s, class: "flex-1 btn"
+          div game.team_a_score.to_s, class: "btn"
         end
-        div "#{ordinal(game.current_end)} End", class: "btn md:btn-lg lg:btn-xl"
-        div "Sheet #{game.sheet}", class: "btn md:btn-lg lg:btn-xl"
+        div "#{ordinal(game.current_end)} End", class: "btn"
+        div "Sheet #{game.sheet}", class: "btn"
         div class: "btn-group flex-1 flex" do
-          div game.team_b_score.to_s, class: "btn md:btn-lg lg:btn-xl"
-          div game.team_b_name.to_s, class: "flex-1 btn md:btn-lg lg:btn-xl"
+          div game.team_b_score.to_s, class: "btn"
+          div game.team_b_name.to_s, class: "flex-1 btn"
           hammer_button if game.team_b_hammer?
         end
       end
