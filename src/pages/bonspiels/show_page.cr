025f6709da251b7480033b521c8d82bba63930ec
Alex Piechowski <alex@piechowski.io>
2022-01-30 17:03:30 -0600

    Style bonspiel meta data

--- src/pages/bonspiels/show_page.cr
+++ src/pages/bonspiels/show_page.cr
@@ -7,6 +7,8 @@ class Bonspiels::ShowPage < MainLayout
     div class: "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" do
       div class: "card shadow-2xl" do
         div class: "card-body" do
+          h2 bonspiel.name, class: "card-title"
+          h3 "Actions"
           render_actions
           render_bonspiel_fields
         end
@@ -47,9 +49,14 @@ class Bonspiels::ShowPage < MainLayout
   end
 
   def render_bonspiel_fields
-    strong bonspiel.start_at.in(Time::Location.load("America/Chicago")).to_s("%a %b %-d %Y")
-    text " to "
-    strong bonspiel.end_at.in(Time::Location.load("America/Chicago")).to_s("%a %b %-d %Y")
+    h3 "Dates"
+    para do
+      strong bonspiel.start_at.in(Time::Location.load("America/Chicago")).to_s("%a %b %-d %Y")
+      text " to "
+      strong bonspiel.end_at.in(Time::Location.load("America/Chicago")).to_s("%a %b %-d %Y")
+    end
+    h3 "Sheets"
+    para bonspiel.sheets
   end
 
   def render_stream_links
