cbd0782f9bd0d01cf90ab907723ddce3c4805653
Alex Piechowski <alex@piechowski.io>
2022-01-30 19:11:01 -0600

    Make finalize disable scoring and ends
    Resolves #18

--- src/components/games/edit_card.cr
+++ src/components/games/edit_card.cr
@@ -10,53 +10,57 @@ class Games::EditCard < BaseComponent
           form_for Games::Presenting::Update.with(game.id), data_turbo_frame: "draw-#{draw.id}-games" do
             if game.presenting?
               hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: ""
-              submit "Present!", class: "btn btn-success"
+              submit "Present!", class: "#{disabled_class} btn btn-success"
             else
               hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: game.id
-              submit "Present!", class: "btn btn-error"
+              submit "Present!", class: "#{disabled_class} btn btn-error"
             end
           end
           div class: "stats md:grid-flow-col grid-flow-row mt-3" do
             div class: "stat place-items-center place-content-center" do
               mount Games::AutoSavingInput, operation.team_a_name, game: game, append_class: "stat-title"
-              mount Games::IncrementingStatValue, operation.team_a_score, game: game
+              mount Games::IncrementingStatValue, operation.team_a_score, game: game, disabled: game.final?
               mount Games::AutoSavingInput, operation.team_a_club_name, game: game, append_class: "stat-desc", placeholder: "Club Name"
               form_for Games::Update.with(game.id), class: "stat-actions" do
                 hidden_input operation.team_a_hammer, value: true
-                submit "Hammer", class: "btn #{game.team_a_hammer? ? "btn-primary" : ""}"
+                submit "Hammer", class: "#{disabled_class} btn #{game.team_a_hammer? ? "btn-primary" : ""}"
               end
             end
             div class: "stat place-items-center place-content-center order-last md:order-none" do
               div "End", class: "stat-title"
-              mount Games::IncrementingStatValue, operation.current_end, game: game
+              mount Games::IncrementingStatValue, operation.current_end, game: game, disabled: game.final?
               div class: "stat-desc" do
                 text "Good Curling!"
               end
               if game.final?
                 form_for Games::Update.with(game.id), class: "stat-actions" do
                   hidden_input operation.final, value: false
-                  submit "Unfinalize", class: "btn btn-ghost"
+                  submit "Unfinalize", class: "btn btn-ghost", data_confirm: "Are you sure you want to unfinalize this game?"
                 end
               else
                 form_for Games::Update.with(game.id), class: "stat-actions" do
                   hidden_input operation.final, value: true
-                  submit "Finalize", class: "btn btn-secondary", data_confirm: "Are you sure?"
+                  submit "Finalize", class: "btn btn-secondary", data_confirm: "Are you sure you want to finalize this game?"
                 end
               end
             end
             div class: "stat place-items-center place-content-center" do
               mount Games::AutoSavingInput, operation.team_b_name, game: game, append_class: "stat-title"
-              mount Games::IncrementingStatValue, operation.team_b_score, game: game
+              mount Games::IncrementingStatValue, operation.team_b_score, game: game, disabled: game.final?
               mount Games::AutoSavingInput, operation.team_b_club_name, game: game, append_class: "stat-desc", placeholder: "Club Name"
               form_for Games::Update.with(game.id), class: "stat-actions" do
                 hidden_input operation.team_a_hammer, value: false
-                submit "Hammer", class: "btn #{game.team_b_hammer? ? "btn-primary" : ""}"
+                submit "Hammer", class: "btn #{game.team_b_hammer? ? "btn-primary" : ""}", disabled: game.final?
               end
             end
           end
         end
       end
     end
+  end
+
+  private def disabled_class : String
+    game.final? ? "btn-disabled" : ""
   end
 
   private def draw : Draw
--- src/components/games/incrementing_stat_value.cr
+++ src/components/games/incrementing_stat_value.cr
@@ -1,19 +1,24 @@
 class Games::IncrementingStatValue(T) < BaseComponent
   needs attribute : Avram::PermittedAttribute(T)
   needs game : Game
+  needs disabled : Bool = false
 
   def render
     div class: "stat-value" do
       form_for Games::Update.with(game.id), class: "inline align-middle" do
         hidden_input attribute, value: Math.max(value - 1, 0)
-        submit "-", class: "btn btn-circle"
+        submit "-", class: "btn btn-circle #{disabled_class}"
       end
       span value, class: "align-middle mx-2"
       form_for Games::Update.with(game.id), class: "inline align-middle" do
         hidden_input attribute, value: value + 1
-        submit "+", class: "btn btn-circle"
+        submit "+", class: "btn btn-circle #{disabled_class}"
       end
     end
+  end
+
+  def disabled_class : String
+    disabled? ? "btn-disabled" : ""
   end
 
   private def value
