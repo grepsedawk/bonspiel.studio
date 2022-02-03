00b857ae8175eed711d40fba5a3aff42c0cbd413
Alex Piechowski <alex@piechowski.io>
2022-02-03 16:44:39 -0600

    Rearrange ticker

--- src/pages/bonspiels/ticker/show_page.cr
+++ src/pages/bonspiels/ticker/show_page.cr
@@ -2,17 +2,17 @@ class Bonspiels::Ticker::ShowPage < Bonspiels::Scoreboard::IndexPage
   def content
     tag "turbo-frame", id: "game-scoreboard-#{game.id}" do
       div class: "scoreboard flex text-center gap-2 animate-slide-in" do
+        div "Sheet #{game.sheet}", class: "btn"
+        div "End #{game.current_end}", class: "btn animate-none"
         div class: "btn-group flex-1 flex flex-nowrap" do
-          hammer_button if game.team_a_hammer?
           div game.team_a_name.to_s, class: "flex-1 btn"
+          hammer_button if game.team_a_hammer?
           div game.team_a_score.to_s, class: "btn"
         end
-        div current_end_display(game.current_end), class: "btn animate-none"
-        div "Sheet #{game.sheet}", class: "btn"
         div class: "btn-group flex-1 flex flex-nowrap" do
-          div game.team_b_score.to_s, class: "btn"
           div game.team_b_name.to_s, class: "flex-1 btn"
           hammer_button if game.team_b_hammer?
+          div game.team_b_score.to_s, class: "btn"
         end
       end
     end
