e66ac5993a6234799d9cfb8272b61b782353858a
Alex Piechowski <alex@piechowski.io>
2022-02-03 21:51:17 -0600

    Add truncating of team name into ticker

--- src/pages/bonspiels/ticker/show_page.cr
+++ src/pages/bonspiels/ticker/show_page.cr
@@ -1,16 +1,20 @@
 class Bonspiels::Ticker::ShowPage < Bonspiels::Scoreboard::IndexPage
   def content
     tag "turbo-frame", id: "game-scoreboard-#{game.id}" do
-      div class: "scoreboard flex text-center gap-2 animate-slide-in" do
+      div class: "scoreboard flex gap-2 animate-slide-in w-screen" do
         div "Sheet #{game.sheet}", class: "btn"
-        div "End #{game.current_end}", class: "btn animate-none"
-        div class: "btn-group flex-1 flex flex-nowrap" do
-          div game.team_a_name.to_s, class: "flex-1 btn"
+        div "End #{game.current_end}", class: "btn"
+        div class: "btn-group min-w-0 flex-1 flex-nowrap" do
+          div class: "flex-1 min-w-0 btn" do
+            div game.team_a_name.to_s, class: "truncate"
+          end
           hammer_button if game.team_a_hammer?
           div game.team_a_score.to_s, class: "btn"
         end
-        div class: "btn-group flex-1 flex flex-nowrap" do
-          div game.team_b_name.to_s, class: "flex-1 btn"
+        div class: "btn-group min-w-0 flex-1 flex-nowrap" do
+          div class: "flex-1 min-w-0 btn" do
+            div game.team_b_name.to_s, class: "truncate"
+          end
           hammer_button if game.team_b_hammer?
           div game.team_b_score.to_s, class: "btn"
         end
