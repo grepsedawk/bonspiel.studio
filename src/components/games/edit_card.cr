d8b3ca43bb3a4bdd63549f2c91eede11e7fc69ef
Alex Piechowski <alex@piechowski.io>
2022-02-04 12:49:09 -0600

    Ultra compress the edit cards

--- src/components/games/edit_card.cr
+++ src/components/games/edit_card.cr
@@ -4,49 +4,47 @@ class Games::EditCard < BaseComponent
 
   def render
     tag "turbo-frame", id: "game-#{game.id}" do
-      div class: "card shadow bg-base-100 mt-6" do
-        div class: "card-body" do
-          div class: "stats md:grid-flow-col grid-flow-row mt-3" do
-          h2 "Sheet #{game.sheet}", class: "card-title"
-            div class: "stat place-items-center place-content-center" do
-              mount Games::AutoSavingInput, operation.team_a_name, game: game, append_class: "stat-title"
-              mount Games::IncrementingStatValue, operation.team_a_score, game: game, disabled: game.final?
-              form_for Games::Update.with(game.id), class: "stat-actions" do
-                hidden_input operation.team_a_hammer, value: true
-                submit "Hammer", class: "#{disabled_class} btn #{game.team_a_hammer? ? "btn-primary" : ""}"
-              end
+      div class: "flex mt-3" do
+        h2 "Sheet #{game.sheet}", class: "card-title"
+        div class: "stats md:grid-flow-col grid-flow-row mt-3" do
+          div class: "stat place-items-center place-content-center" do
+            mount Games::AutoSavingInput, operation.team_a_name, game: game, append_class: "stat-title"
+            mount Games::IncrementingStatValue, operation.team_a_score, game: game, disabled: game.final?
+            form_for Games::Update.with(game.id), class: "stat-actions" do
+              hidden_input operation.team_a_hammer, value: true
+              submit "Hammer", class: "#{disabled_class} btn #{game.team_a_hammer? ? "btn-primary" : ""}"
             end
-            div class: "stat place-items-center place-content-center order-last md:order-none" do
-              div "End", class: "stat-title"
-              mount Games::IncrementingStatValue, operation.current_end, game: game, disabled: game.final?
-              div class: "stat-actions" do
-                form_for Games::Update.with(game.id), class: "inline" do
-                  if game.final?
-                    hidden_input operation.final, value: false
-                    submit "Unfinalize", class: "btn btn-ghost", data_confirm: "Are you sure you want to unfinalize this game?"
-                  else
-                    hidden_input operation.final, value: true
-                    submit "Finalize", class: "btn btn-secondary", data_confirm: "Are you sure you want to finalize this game?"
-                  end
+          end
+          div class: "stat place-items-center place-content-center order-last md:order-none" do
+            div "End", class: "stat-title"
+            mount Games::IncrementingStatValue, operation.current_end, game: game, disabled: game.final?
+            div class: "stat-actions" do
+              form_for Games::Update.with(game.id), class: "inline" do
+                if game.final?
+                  hidden_input operation.final, value: false
+                  submit "Unfinalize", class: "btn btn-ghost", data_confirm: "Are you sure you want to unfinalize this game?"
+                else
+                  hidden_input operation.final, value: true
+                  submit "Finalize", class: "btn btn-secondary", data_confirm: "Are you sure you want to finalize this game?"
                 end
-                form_for Games::Presenting::Update.with(game.id), data_turbo_frame: "draw-#{draw.id}-games", class: "inline" do
-                  if game.presenting?
-                    hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: ""
-                    submit "Present!", class: "btn btn-success"
-                  else
-                    hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: game.id
-                    submit "Present!", class: "btn btn-error"
-                  end
+              end
+              form_for Games::Presenting::Update.with(game.id), data_turbo_frame: "draw-#{draw.id}-games", class: "inline" do
+                if game.presenting?
+                  hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: ""
+                  submit "Present!", class: "btn btn-success"
+                else
+                  hidden_input SaveBonspiel.new(game.bonspiel!).presenting_game_id, value: game.id
+                  submit "Present!", class: "btn btn-error"
                 end
               end
             end
-            div class: "stat place-items-center place-content-center" do
-              mount Games::AutoSavingInput, operation.team_b_name, game: game, append_class: "stat-title"
-              mount Games::IncrementingStatValue, operation.team_b_score, game: game, disabled: game.final?
-              form_for Games::Update.with(game.id), class: "stat-actions" do
-                hidden_input operation.team_a_hammer, value: false
-                submit "Hammer", class: "btn #{game.team_b_hammer? ? "btn-primary" : ""} #{disabled_class}"
-              end
+          end
+          div class: "stat place-items-center place-content-center" do
+            mount Games::AutoSavingInput, operation.team_b_name, game: game, append_class: "stat-title"
+            mount Games::IncrementingStatValue, operation.team_b_score, game: game, disabled: game.final?
+            form_for Games::Update.with(game.id), class: "stat-actions" do
+              hidden_input operation.team_a_hammer, value: false
+              submit "Hammer", class: "btn #{game.team_b_hammer? ? "btn-primary" : ""} #{disabled_class}"
             end
           end
         end
