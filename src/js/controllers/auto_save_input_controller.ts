21851a06fa8b6319d8eb4f12563bea4fbd107d96
Alex Piechowski <alex@piechowski.io>
2022-02-04 21:58:19 -0600

    Select all text in game edit card when editing teams
    When you edit a team name, you're usually changing the whole name
    not fixing a typo or whatnot
    
    Thanks Austin

--- src/js/controllers/auto_save_input_controller.ts
+++ src/js/controllers/auto_save_input_controller.ts
@@ -4,4 +4,8 @@ export default class extends Controller {
   public submit(e): void {
     e.target.closest("form").requestSubmit()
   }
+
+  public selectAll(e): void {
+    e.target.select()
+  }
 }
