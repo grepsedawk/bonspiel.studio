3ab16814156f248e0115577c09929017c3db01ee
Alex Piechowski <alex@piechowski.io>
2023-02-03 21:19:16 -0800

    Allow user to pick the currently shown sheet

--- src/actions/bonspiels/scoreboard/index.cr
+++ src/actions/bonspiels/scoreboard/index.cr
@@ -1,8 +1,14 @@
 class Bonspiels::Scoreboard::Index < BrowserAction
   include Auth::AllowGuests
 
-  get "/bonspiels/:bonspiel_id/scoreboard" do
+  get "/bonspiels/:bonspiel_id/scoreboard/?:sheet" do
     bonspiel = BonspielQuery.find(bonspiel_id)
+
+    sheet.try(&.to_i).try do |sheet|
+      bonspiel.presenting_game!.try do |game|
+        return html Bonspiels::Scoreboard::IndexPage, game: game.draw!.games![sheet - 1]
+      end
+    end
 
     bonspiel.presenting_game!.try do |game|
       return html Bonspiels::Scoreboard::IndexPage, game: game
